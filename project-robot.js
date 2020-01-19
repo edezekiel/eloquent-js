138b78d901a77fa24994afdf411e18c4dbc8bbff
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2020-01-19 08:26:54 -0600

    Refactor robot measurement solution'

--- project-robot.js
+++ project-robot.js
@@ -226,18 +226,14 @@ function compareRobots(robot1, memory1, robot2, memory2) {
 
   for (let i = 0; i < 100; i++) {
     let randomVillage = VillageState.random();
-    let robot1Steps = runRobot(randomVillage, robot1, memory1);
-    let robot2Steps = runRobot(randomVillage, robot2, memory2);
-    if (i === 0) {
-      averageSteps[0] = robot1Steps;
-      averageSteps[1] = robot2Steps;
-    } else {
-      averageSteps[0] = (averageSteps[0] + robot1Steps)/2;
-      averageSteps[1] = (averageSteps[1] + robot2Steps)/2;
-    }
+    averageSteps[0] += runRobot(randomVillage, robot1, memory1);
+    averageSteps[1] += runRobot(randomVillage, robot2, memory2);
   }
+
+  averageSteps[0] = averageSteps[0]/100;
+  averageSteps[1] = averageSteps[1]/100;
+
   console.log(averageSteps);
-  return averageSteps;
 }
 
 compareRobots(routeRobot, [], goalOrientedRobot, []);
