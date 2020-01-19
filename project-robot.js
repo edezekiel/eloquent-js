da80b4ffebf53b4fae7c4ebd99a5c944b2263b89
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2020-01-19 09:06:39 -0600

    Update comment to exercise solution

--- project-robot.js
+++ project-robot.js
@@ -224,7 +224,7 @@ function smarterRobot({place, parcels}, route) {
     let parcel = parcels[0];
 
     // check if any parcels' delivery address is the starting place
-    // slightly faster than randomly picking first parcel in array
+    // slightly faster than simply picking first parcel in array
     // TODO - set first parcel to nearest parcel
     for (p of parcels) {
       if (p.address === place) {
@@ -232,7 +232,7 @@ function smarterRobot({place, parcels}, route) {
         console.log(parcel)
       }
     }
-    
+
     if (parcel.place != place) {
       route = findRoute(roadGraph, place, parcel.place);
     } else {
