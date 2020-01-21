ec2e1524e0f7084cdc613c207e0e6febb664eaf2
Ed Ezekiel <ed@ubiquisoft.com>
2020-01-21 14:44:58 -0600

    Create solution to PGroup exercise

---
+++ PGroup.js
@@ -0,0 +1,38 @@
+class PGroup {
+    constructor(arr) {
+      this.entries = arr;
+  }
+
+  add(val) {
+      if (!(this.has(val))) {
+          return new PGroup(this.entries.concat(val));
+      }
+  }
+
+  delete(val) {
+      if ((this.has(val))) {
+          return new PGroup(this.entries.filter(entry => entry !== val));
+      }
+  }
+
+  has(val) {
+      return this.entries.includes(val);
+  }
+
+  static empty = new PGroup([]);
+
+}
+
+let a = PGroup.empty.add("a");
+console.log(a)
+let ab = a.add("b");
+console.log(ab)
+let b = ab.delete("a");
+console.log(b)
+
+console.log(b.has("b"));
+// → true
+console.log(a.has("b"));
+// → false
+console.log(b.has("a"));
+// → false
