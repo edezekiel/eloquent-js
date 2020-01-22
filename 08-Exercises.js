639e8bae4b46035cdc8b51530ef345a457726a63
Ed Ezekiel <ed@ubiquisoft.com>
2020-01-22 08:30:11 -0600

    Solution to Chapter 8, locked box exercise

--- 08-Exercises.js
+++ 08-Exercises.js
@@ -19,5 +19,50 @@ function reliableMultiply(a, b) {
   }
 }
 
-console.log(reliableMultiply(8, 8));
+// console.log(reliableMultiply(8, 8));
 // → 64
+
+const box = {
+    locked: true,
+    unlock() { this.locked = false; }, 
+    lock() { this.locked = true; },
+    _content: [],
+    get content() {
+        if (this.locked) throw new Error("Locked");
+        return this._content;
+    }
+};
+
+const withBoxUnlocked = (body) => {
+    let progress = 0;
+    try {
+        // unlock the box
+        box.unlock();
+        progress = 1;
+        // run the function
+        body();
+        progress = 2;
+        // ensure box is locked before returning, whether or not body() succeeded
+        box.lock(); 
+    } catch (e) {
+        console.log(e);
+        if (progress === 1) {
+            box.lock();
+        }
+    }
+}
+
+withBoxUnlocked(function() {
+    box.content.push("gold piece");
+});
+
+try {
+    withBoxUnlocked(function() {
+        throw new Error("Pirates on the horizon! Abort!");
+    });
+} catch (e) {
+    console.log("Error raised: " + e);
+}
+
+console.log(box.locked);
+// -> true
