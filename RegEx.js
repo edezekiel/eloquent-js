15b87d5c9b6c93edb52e77a21b0dad9caa6f82d1
Ed Ezekiel <ed@ubiquisoft.com>
2020-01-24 13:10:11 -0600

    Answers to RegEx exercise 1

--- RegEx.js
+++ RegEx.js
@@ -1,10 +1,38 @@
-let re1 = new RegExp('abc');
-let re2 = /abc/;
-let eighteenPlus = /eighteen\+/;
-let dateTime = /\d\d-\d\d-\d\d\d\d \d\d:\d\d/;
-let notBinary = /[^01]/;
-console.log(/'\d*'/.test("'123'"));
-console.log(/'\d*'/.test("''"))
-let neighbor = /neighbou?r/;
-console.log(neighbor.test("neighbour"))
-console.log(neighbor.test("neighbor"))
+// let re1 = new RegExp('abc');
+// let re2 = /abc/;
+// let eighteenPlus = /eighteen\+/;
+// let dateTime = /\d\d-\d\d-\d\d\d\d \d\d:\d\d/;
+// let notBinary = /[^01]/;
+// console.log(/'\d*'/.test("'123'"));
+// console.log(/'\d*'/.test("''"));
+// let neighbor = /neighbou?r/;
+// console.log(neighbor.test("neighbour"));
+// console.log(neighbor.test("neighbor"));
+
+// function getDate(string) {
+//     let [_, month, day, year] = /^(\d{1,2})-(\d{1,2})-(\d{4})$/.exec(string);
+//     return new Date(year, month - 1, day);
+// }
+
+// ******************RegExp Golf**************************
+
+// car and cat
+let carCatRegExp = /car|cat/
+
+// pop and prop
+let popPropRegExp = /pop|prop/
+
+// ferret, ferry, and ferrari
+let ferRegExp = /ferret|ferry|ferrari/
+
+// Any word ending in ious
+let iousRegExp = /\b\w+ious$\b/
+
+// A whitespace character followed by a period, comma, colon, or semicolon
+let wspcRegEx = /\s(\.|,|:|;)+/
+
+// A word longer than six letters
+let sixRegExp = /\b\w{6}\w+\b/
+
+// A word without the letter e (or E)
+let notERegExp = /\b([^e\s]+)\b/
