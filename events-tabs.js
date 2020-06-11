7e509bfef5da01936f8338b8a81d10a18ed61662
Ed Ezekiel <ed@ubiquisoft.com>
2020-06-11 14:41:06 -0500

    Compare my tab solution to eloquent js solution

--- events-tabs.js
+++ events-tabs.js
@@ -1,4 +1,31 @@
+// Eloquent JS solution (tweaked to node.data.tabname)
 
+function asTabs(node) {
+  const tabs = Array.from(node.children).map(node => {
+    let button = document.createElement('button');
+    button.textContent = node.dataset.tabname;
+    let tab = { node, button };
+    button.addEventListener('click', () => selectTab(tab));
+    return tab;
+  });
+
+  let tabList = document.createElement("div");
+  for (let { button } of tabs) tabList.appendChild(button);
+  node.insertBefore(tabList, node.firstChild);
+
+  function selectTab(selectedTab) {
+    for (let tab of tabs) {
+      let selected = tab == selectedTab;
+      tab.node.style.display = selected ? "" : "none";
+      tab.button.style.color = selected ? "red" : "";
+    }
+  }
+  selectTab(tabs[0]);
+}
+
+asTabs(document.querySelector("tab-panel"));
+
+// My original solution
 function asTabs(node) {
   const tabs = Array.from(node.children);
   tabs.forEach(tab => tab.style.display = 'none');
