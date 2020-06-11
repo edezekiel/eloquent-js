38260ab0d37c9f698a6c2ffb70594354d9bc2ba0
Ed Ezekiel <ed@ubiquisoft.com>
2020-06-11 14:27:04 -0500

    Code solution to part of the tabs exercise

---
+++ events-tabs.js
@@ -0,0 +1,31 @@
+function asTabs(node) {
+  const tabs = Array.from(node.children);
+  tabs.forEach(tab => tab.style.display = 'none');
+  const tabLength = tabs.length;
+  let buttons = [];
+
+  for (let i = tabLength - 1; i > -1; i--) {
+
+    let button = document.createElement('button');
+    button.textContent = tabs[i].dataset.tabname;
+
+    button.addEventListener('click', (event) => {
+      const name = event.target.textContent;
+      tabs.forEach(tab => {
+        if (name === tab.dataset.tabname) {
+          tab.style.display = 'block';
+          button.style.color = 'red';
+        } else {
+          tab.style.color = 'none';
+        }
+      })
+
+    });
+
+    buttons.push(button)
+  }
+
+  buttons.forEach(button => node.prepend(button));
+}
+asTabs(document.querySelector("tab-panel"));
+
